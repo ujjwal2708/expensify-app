63a1067ffaa300dfa4ddf3c5536cb27e7aad7910
Ujjwal Agarwal <ujjwalrbi9@gmail.com>
2019-04-22 18:25:29 +0530

    Setup production webpack build

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Git Command
+
+git init : Create a new git repo
+
+git status : View changes to your project code
+
+git add = add files to staging area
+
+git commit - Create a new commit with files from staging area
+
+git log : View list of recent commits
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,56 @@
 // entry --> output
 const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = {
-  entry: "./src/app.js",
-  output: {
-    path: path.join(__dirname, "public"), // needs to be a absolute path
-    filename: "bundle.js"
-  },
+module.exports = env => {
+  const isProduction = env === "production";
+  const CSSExtract = new ExtractTextPlugin("styles.css");
 
-  module: {
-    rules: [
-      {
-        loader: "babel-loader",
-        test: /\.js$/,
-        exclude: /node_modules/
-      },
-      {
-        test: /\.s?css$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      }
-    ]
-  },
+  return {
+    entry: "./src/app.js",
+    output: {
+      path: path.join(__dirname, "public"), // needs to be a absolute path
+      filename: "bundle.js"
+    },
 
-  mode: "none",
+    module: {
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.s?css$/,
+          use: CSSExtract.extract({
+            use: [
+              {
+                loader: "css-loader",
+                options: {
+                  sourceMap: true
+                }
+              },
+              {
+                loader: "sass-loader",
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+          })
+        }
+      ]
+    },
 
-  devtool: "cheap-module-eval-source-map", // helps in faster debugging
+    plugins: [CSSExtract],
 
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-    historyApiFallback: true
-  }
+    mode: "none",
+
+    devtool: isProduction ? "source-map" : "inline-source-map", // helps in faster debugging
+
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      historyApiFallback: true
+    }
+  };
 };
