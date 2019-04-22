b0a082f460d77231c7099774f0b5a572f855982a
Ujjwal Agarwal <ujjwalrbi9@gmail.com>
2019-04-22 16:31:49 +0530

    Removed Dummy Empty data

--- src/app.js
+++ src/app.js
@@ -20,29 +20,29 @@ import "./styles/style.scss";
 
 const store = configureStore();
 
-store.dispatch(
-  addExpense({
-    description: "Water Bill",
-    amount: 4500
-  })
-);
+// store.dispatch(
+//   addExpense({
+//     description: "Water Bill",
+//     amount: 4500
+//   })
+// );
 
-store.dispatch(
-  addExpense({
-    description: "Gas Bill",
-    createdAt: 1000
-  })
-);
-store.dispatch(
-  addExpense({
-    description: "Rent",
-    amount: 109500
-  })
-);
+// store.dispatch(
+//   addExpense({
+//     description: "Gas Bill",
+//     createdAt: 1000
+//   })
+// );
+// store.dispatch(
+//   addExpense({
+//     description: "Rent",
+//     amount: 109500
+//   })
+// );
 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
