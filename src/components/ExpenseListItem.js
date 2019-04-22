0373fab23e9a800abb1650c51b0ef630c01b7125
Ujjwal Agarwal <ujjwalrbi9@gmail.com>
2019-04-22 19:22:28 +0530

    setup formatting for amount and createdAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import moment from "moment";
+import numeral from "numeral";
+
+// load a locale
+numeral.register("locale", "fr", {
+  delimiters: {
+    thousands: ",",
+    decimal: "."
+  },
+  abbreviations: {
+    thousand: "k",
+    million: "m",
+    billion: "b",
+    trillion: "t"
+  },
+  ordinal: function(number) {
+    return number === 1 ? "er" : "ème";
+  },
+  currency: {
+    symbol: "Rs. "
+  }
+});
+
+// switch between locales
+numeral.locale("fr");
 
 const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
   <div>
@@ -7,7 +32,8 @@ const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
       <h3>{description}</h3>
     </Link>
     <p>
-      {amount} - {createdAt}
+      {numeral(amount / 100).format("$0,0.00")}-
+      {moment(createdAt).format("MMMM Do, YYYY")}
     </p>
   </div>
 );
